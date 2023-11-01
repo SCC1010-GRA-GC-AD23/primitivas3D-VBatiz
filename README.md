d5e6964a28e4c422e494853e0e3043c14dad3175
Víctor Manuel Bátiz Beltrán <51245128+vbatiz@users.noreply.github.com>
2023-11-01 22:51:51 +0000

    Terminamos el programa

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# Plantilla3D
+# Plantilla3
+D
 Plantilla base para proyectos en 3D con P5.JS
--- sketch.js
+++ sketch.js
@@ -1,5 +1,8 @@
+let texturaTierra
+
 function preload() {
   // put preload code here
+  texturaTierra = loadImage("./images/tierra.jpg")
 }
 
 function setup() {
@@ -10,9 +13,75 @@ function setup() {
 function draw() {
   // put drawing code here
   background(255)
-  translate(-450,-250,0)
+  translate(-450,-200,0)
+
+  push()
+  fill(255,0,0)
   rotateX(frameCount * 0.01)
   rotateY(frameCount * 0.01)
   rotateZ(frameCount * 0.01)
+  texture(texturaTierra)
   plane(160, 60)
+  pop()
+
+  translate(200,0,0)
+  push()
+  fill(0,0,255)
+  rotateX(frameCount * 0.01)
+  rotateY(frameCount * 0.01)
+  rotateZ(frameCount * 0.01)
+  texture(texturaTierra)
+  sphere(80,60)
+  pop()
+
+  translate(200,0,0)
+  push()
+  rotateX(frameCount * 0.01)
+  rotateY(frameCount * 0.01)
+  rotateZ(frameCount * 0.01)
+  texture(texturaTierra)
+  noStroke()
+  box(80,60, 40)
+  pop()
+
+  translate(200,0,0)
+  push()
+  rotateX(frameCount * 0.01)
+  rotateY(frameCount * 0.01)
+  rotateZ(frameCount * 0.01)
+  texture(texturaTierra)
+  noStroke()
+  torus(60,40)
+  pop()
+
+  translate(-600,250,0)
+  push()
+  rotateX(frameCount * 0.01)
+  rotateY(frameCount * 0.01)
+  rotateZ(frameCount * 0.01)
+  texture(texturaTierra)
+  noStroke()
+  cone(80, 120)
+  pop()
+  
+  translate(200,0,0)
+  push()
+  rotateX(frameCount * 0.01)
+  rotateY(frameCount * 0.01)
+  rotateZ(frameCount * 0.01)
+  texture(texturaTierra)
+  noStroke()
+  cylinder(60,120)
+  pop()
+
+  translate(200,0,0)
+  push()
+  rotateX(frameCount * 0.01)
+  rotateY(frameCount * 0.01)
+  //rotateZ(frameCount * 0.01)
+  texture(texturaTierra)
+  noStroke()
+  ellipsoid(80,40)
+  pop()
+
 }
